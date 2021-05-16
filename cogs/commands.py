2636e40979f3c1158c21ff788155a2e0ad85c65e
S3cBar0n <secberg@gmail.com>
2021-05-16 17:38:16 -0600

    Moved the user command to the gitconnect.py to keep all DB and Github related commands together.

--- cogs/commands.py
+++ cogs/commands.py
@@ -19,15 +19,6 @@ class Commands(commands.Cog):
     async def roll(self, ctx):
         await ctx.send(random.randint(1, 6))
 
-    @commands.command(aliases=["user", "info"])
-    # @commands.has_permissions(kick_members=True)
-    async def whois(self, ctx, member: discord.Member):
-        embed = discord.Embed(title=member.name, description=member.mention, color=discord.Colour.green())
-        embed.add_field(name='ID', value=member.id, inline=True)
-        embed.set_thumbnail(url=member.avatar_url)
-        embed.set_footer(icon_url=ctx.author.avatar_url, text=f"Requested by {ctx.author.name}")
-        await ctx.send(embed=embed)
-
 
 # This function allows us to connect this cog to our bot
 def setup(client):
